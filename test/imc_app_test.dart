bf9927872327213fc198d79857ab65b69358ef0c
reynatojunior <reynato.junior@hotmail.com>
2023-08-30 09:27:43 -0300

    commit com testes

--- test/imc_app_test.dart
+++ test/imc_app_test.dart
@@ -1,8 +1,8 @@
-import 'package:imc_app/imc_app.dart';
+import 'package:imc_app/functions/func.dart' as app;
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(42, 42);
+  test('Testar calculo IMC', () {
+    expect(app.imc(10, 10), 1000);
   });
 }
